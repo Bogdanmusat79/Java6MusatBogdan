f2117ab03f1013f73ad291d71bd7a29178681d29
Bogdan <Bogdan@Bogdan-PC>
2017-09-16 19:20:21 +0300

    This is my first program in Java

---
+++ Java6MusatBogdan/src/Computer/Calculator.java
@@ -0,0 +1,41 @@
+/*
+ *Computer frame
+ * and open the template in the editor.
+ */
+package Computer;
+/**
+ *computer frame
+ * @author Bogdan
+ */
+public class Calculator {
+
+    /**
+     * Methods adds two digits introduced by user from keyboard
+     */
+    public void add(){
+    }
+    
+    /**
+     * Methods decrease two numbers introduced by user from keyboard
+     */
+    public void decrease(){
+    }
+    
+    /**
+     * Methods multiply two numbers introduced by user from keyboard
+     */
+    public void multiply (){
+    }
+    
+    /**
+     * Methods divide two numbers introduced by user from keyboard
+     */
+    public void divide (){
+    }
+    
+    /**
+     * Start the Calculator
+     */
+     public void start (){
+     }
+}
