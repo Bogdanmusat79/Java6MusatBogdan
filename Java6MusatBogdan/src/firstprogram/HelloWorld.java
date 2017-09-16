2c8d7c2132725fb4258f1a43f87f99c1d17eda56
Bogdan <Bogdan@Bogdan-PC>
2017-09-16 17:04:48 +0300

    This is my first program in Java

---
+++ Java6MusatBogdan/src/firstprogram/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * My first program 
+ * and open the template in the editor.
+ */
+package firstprogram;
+
+/**
+ *This is my first class
+ * @author Bogdan
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // say hello world  
+        System.out.println("Hello World!");
+    }
+    
+}
