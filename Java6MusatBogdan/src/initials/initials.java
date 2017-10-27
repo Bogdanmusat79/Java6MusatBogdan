c22d0ec761878c9c2af18484d11623c7484e4b73
Bogdan <Bogdan@Bogdan-PC>
2017-10-27 21:25:21 +0300

    show initials

---
+++ Java6MusatBogdan/src/initials/initials.java
@@ -0,0 +1,32 @@
+/*
+ * Show name initials
+ */
+package initials;
+
+/**
+ *
+ * @author Bogdan
+ */
+public class initials {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String name = "Bogdan Musat";
+    //define neme
+    StringBuffer initials = new StringBuffer();
+    //insert string buffer
+        int length = name.length();
+        for (int i = 0; i < length; i++) {
+            if (Character.isUpperCase(name.charAt(i))) {
+                initials.append(name.charAt(i));
+                //if char is uppercase append
+            }
+        }
+        System.out.println("My initials are: " + initials);
+    }
+
+    }
+    
+
