b2905d98fb27fc6af94f4f1630c2f8f28ed53777
gheor <gheor@SURFACE-PRO4-AP>
2017-11-22 19:50:48 +0200

    Corrected example with array

--- Java6MusatBogdan/src/array/ElectronicBook.java
+++ Java6MusatBogdan/src/array/ElectronicBook.java
@@ -14,22 +14,11 @@ import array.EBook;
  * @author Bogdan
  */
 
-public class ElectronicBook {
-    public static void main(String[] args) {
-    //define Books
-     EBook eb1 = new EBook("Seara pe deal","1234567891012","Mihai Eminescu","www.mystore.ro/seara","bestsell","nemira",20.00,"ebook","1885");
-     EBook eb2 = new EBook("Zdreanta","123222789101222","Tudor Arghezi","www.mystore.ro/zdreanta","bestbuy","libris",15.00,"ebook","19542");
-     IBook ib1 = new IBook("El Zorab","1233337891012","George Cosbuc","www.mystore.ro/Elzorab","bestprice","art",17.00,"ibook","1189");
-
-//create Array
+public abstract class ElectronicBook {
     
-     ElectronicBook[] carti = {eb1,eb2,ib1};
-    
-     //method for print
-    for(ElectronicBook currentEBook : carti){
-            currentEBook.printElectronicBook();
-       }
-}
+    // ai uitat sa copiezi in ElectronicBooks si metoda abastracta care
+    // iti lipsea si care a fost implementata in cele doua clase derivate
+    abstract void printElectronicBook();
 }
    
 
